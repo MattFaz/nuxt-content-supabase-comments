8ae020ac65d1cd64196f898bb1eccca7f4f47712
Matt Farrell <mfarrell@squareup.com>
2023-03-10 08:23:05 +1100

    remove ssr option

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: true,
+    // ssr: true,
     modules: ['@nuxt/content', '@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
     content: {
         // https://content.nuxtjs.org/api/configuration
