848c97e80544dc4487f922b38dd9379b7eca4c52
Matt Farrell <mfarrell@squareup.com>
2023-03-10 08:25:14 +1100

    Merge static branch (#1)
    * nuxt.config additions
    
    * remove ssr option

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: true,
+    // ssr: true,
     modules: ['@nuxt/content', '@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
     content: {
         // https://content.nuxtjs.org/api/configuration
@@ -10,5 +10,14 @@ export default defineNuxtConfig({
     },
     tailwindcss: {
         // https://tailwindcss.nuxtjs.org/getting-started/options
-    }
+    },
+    dir: {
+        public: "public"
+    },
+    runtimeConfig: {
+        public: {
+            SUPABASE_URL: process.env.SUPABASE_URL,
+            SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+        },
+    },
 })
